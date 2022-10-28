07bdc82460bcb8f4613e1af73e11277581d6e162
Yure <49767009+YureDomingues@users.noreply.github.com>
2022-10-28 08:41:19 -0300

    Implementação do mergeSort

--- merge_sort.h
+++ merge_sort.h
@@ -1,5 +1,7 @@
-#ifndef _FUNCOES_GERAIS_H
-#define _FUNCOES_GERAIS_H
+#ifndef _MERGE_SORT_H
+#define _MERGE_SORT_H
 
+void merge(int *v, int ini, int meio, int fim);
+void mergeSort(int *v, int ini, int fim);
 
 #endif
